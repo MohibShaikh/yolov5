4c80af7606ded393ab593dcd4591e04488d95056
MohibShaikh <mohibuddin9@gmail.com>
2024-11-19 23:23:39 +0500

    Update val.py
    Signed-off-by: MohibShaikh <mohibuddin9@gmail.com>

--- val.py
+++ val.py
@@ -29,6 +29,7 @@ from pathlib import Path
 import numpy as np
 import torch
 from tqdm import tqdm
+from utils.torch_utils import prune
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
